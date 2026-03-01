f388010e8f7c9897f29c9ebd5849223fcaa49acb
aswin-s-04 <aswinstark007@gmail.com>
2026-03-01 10:45:47 +0530

    updated final bbb

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import GlossyBackground from "./components/GlossyBackground";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Portfolio from "./components/portfolio";
+import Portfolio from "./components/Services";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Faq from "./components/Faq";
+import Services from "./components/Services";
 export default function App() {
   return (
     <GlossyBackground>
       <Header /> {/* Put header *inside* the glossy background */}
       <Hero />
-      <Portfolio />
+      <Services />
       <About />
       <Faq />
       <Contact />

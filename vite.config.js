4ff83d03b955c7fd66534073bf66aa8d4c69f770
aswin-s-04 <aswinstark007@gmail.com>
2026-03-29 12:08:46 +0530

    add CNAME file and update base path comment in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/sagraphics/", // ✅ MUST be string with slashes
+   // ✅ MUST be string with slashes
 })
